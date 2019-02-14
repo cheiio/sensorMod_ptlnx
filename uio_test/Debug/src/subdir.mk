68e2bcd6b1f06d7fcc1bc1dc082d8bf5acc3999d
Sergio Pertuz <sapertuz@gmail.com>
2019-02-13 23:27:29 -0200

    Day Two

--- uio_test/Debug/src/subdir.mk
+++ uio_test/Debug/src/subdir.mk
@@ -4,14 +4,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/SensorMod.c \
 ../src/xil_uio.c \
 ../src/xil_uio_test.c 
 
 OBJS += \
+./src/SensorMod.o \
 ./src/xil_uio.o \
 ./src/xil_uio_test.o 
 
 C_DEPS += \
+./src/SensorMod.d \
 ./src/xil_uio.d \
 ./src/xil_uio_test.d 
 
