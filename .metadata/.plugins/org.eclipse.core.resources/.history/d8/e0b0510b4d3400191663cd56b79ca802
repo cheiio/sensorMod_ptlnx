0563601cc32614254a1a4c8d72b9a3c370321f1e
Sergio Pertuz <sapertuz@gmail.com>
2019-02-19 11:03:48 -0300

    19022019-1103

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d8/e0b0510b4d3400191663cd56b79ca802
@@ -0,0 +1,3 @@
+src/xil_uio.o src/xil_uio.o: ../src/xil_uio.c ../src/xil_uio.h
+
+../src/xil_uio.h:
--- uio_test/Debug/src/xil_uio_test.d
+++
@@ -1,4 +0,0 @@
-src/xil_uio_test.o src/xil_uio_test.o: ../src/xil_uio_test.c \
- ../src/xil_uio.h
-
-../src/xil_uio.h:
